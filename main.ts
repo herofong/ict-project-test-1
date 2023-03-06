0b4bf1965ef983836aa43e56f59568b83ccd8a6f
herofong <126774050+herofong@users.noreply.github.com>
2023-03-06 21:15:55 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,35 @@
-input.onButtonPressed(Button.A, function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showNumber(0)
+    } else if (receivedNumber == 1) {
+        basic.showNumber(1)
+    } else if (receivedNumber == 2) {
+        basic.showNumber(2)
+    } else if (receivedNumber == 3) {
+        basic.showNumber(3)
+    } else if (receivedNumber == 4) {
+        basic.showNumber(4)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showString("Are you sure about that?")
+    basic.clearScreen()
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(subject)
+    } else {
+        control.waitMicros(10000)
+        basic.showNumber(subject)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     if (subject == 4) {
-        subject = 1
+        subject = 0
     } else {
         subject += 1
     }
 })
 let subject = 0
-subject = 1
+subject = 0
 basic.forever(function () {
     basic.showNumber(subject)
 })
